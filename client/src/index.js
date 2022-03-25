74d0ccb5daf73d0adc9f87b6192fc2472046f178
Andrés Viláemail <vilaandres@gmail.com>
2022-03-25 11:06:42 -0500

    Fixed dotend import

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,8 @@ import { Provider } from 'react-redux';
 import { store } from './store'
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-// dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
